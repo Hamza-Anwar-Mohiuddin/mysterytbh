b5285cfa7bc0c22010e767a15dbc8a46ba4e7f11
hmz-mohi <hmzmohi2003@gmail.com>
2024-05-05 17:07:50 +0500

    .

--- src/app/(app)/page.tsx
+++ src/app/(app)/page.tsx
@@ -61,7 +61,7 @@ export default function Home() {
 
       {/* Footer */}
       <footer className="text-center p-4 md:p-6 bg-gray-900 text-white">
-        © 2023 True Feedback. All rights reserved.
+        Whispr Mystery Messages
       </footer>
     </>
   );
